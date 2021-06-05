be4cbf119de773e1c7764c9f74a7742e2bae540c
Jared Mason <Wohiepee@gmail.com>
2021-06-04 23:14:53 -0500

    new commit

--- src/foodnutrtition/Foodmain.java
+++ src/foodnutrtition/Foodmain.java
@@ -6,7 +6,7 @@ import java.util.Scanner;
 import java.util.Timer;
 
 public class Foodmain {
-
+//yeet
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Scanner s = new Scanner(System.in);
